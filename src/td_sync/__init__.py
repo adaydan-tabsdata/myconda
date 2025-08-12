b6e7a884a3386939cd789993c8a7c1feb8244fa9
adaydan-tabsdata <daniel@tabsdata.com>
2025-08-12 13:08:41 -0700

    t06

--- src/td_sync/__init__.py
+++ src/td_sync/__init__.py
@@ -2,4 +2,4 @@ from .sync import main as sync_with_server
 from .cancel_flows import main as cancel_trx
 from .trigger import main as trigger_transaction
 
-__all__ = ["cancel_trx", "sync_with_server, trigger_transaction"]
+__all__ = ["cancel_trx", "sync_with_server", "trigger_transaction"]
