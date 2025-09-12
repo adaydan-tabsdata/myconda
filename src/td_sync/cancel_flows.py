c4cafb89a210bfb51853b7f7ecc39781182d5fa9
adaydan-tabsdata <daniel@tabsdata.com>
2025-09-12 15:46:13 -0700

    html updates to UI and Getting Started

--- src/td_sync/cancel_flows.py
+++ src/td_sync/cancel_flows.py
@@ -10,7 +10,7 @@ import re
 def main():
     server = TabsdataServer("127.0.0.1:2457", "admin", "tabsdata", "sys_admin")
     transactions = server.list_transactions()
-    stalled_transactions = [i for i in transactions if i.status == 'Stalled']
+    stalled_transactions = [i for i in transactions if i.status in ['Stalled', 'Running']]
     if len(stalled_transactions) > 0:
         canceled_transactions = [i.cancel() for i in stalled_transactions]
         print(f'Canceled {len(canceled_transactions)} stalled transactions')
